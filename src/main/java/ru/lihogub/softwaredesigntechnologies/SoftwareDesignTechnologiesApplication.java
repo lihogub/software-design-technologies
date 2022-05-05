ad6e1ebb07115a9898eb49836e492616ecba8561
Lihogub Oleg <oligli@yandex.ru>
2022-05-05 16:27:29 +0300

    init commit

---
+++ src/main/java/ru/lihogub/softwaredesigntechnologies/SoftwareDesignTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package ru.lihogub.softwaredesigntechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDesignTechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDesignTechnologiesApplication.class, args);
+	}
+
+}
